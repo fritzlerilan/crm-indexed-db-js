91017255cda868eec1356dc4e1bac79acab5bd9c
fritzlerilan <fritzlerilan@gmail.com>
2021-10-01 14:18:56 -0300

    create db

---
+++ js/app.js
@@ -0,0 +1,34 @@
+(function () {
+    let DB;
+    window.onload = () => {
+        crearDB();
+    }
+
+    function crearDB() {
+        const crearDB = window.indexedDB.open('crm', 1);
+        crearDB.onerror = () => {
+            console.log('Hubo un error');
+        }
+
+        crearDB.onsuccess = () => {
+            DB = crearDB.result;
+        }
+
+        crearDB.onupgradeneeded = function (e) {
+            const db = e.target.result;
+
+            const objectStore = db.createObjectStore('crm', { keyPath: 'id', autoIncrement: true });
+
+            objectStore.createIndex('nombre', 'nombre', { unique: false });
+            objectStore.createIndex('email', 'email', { unique: true });
+            objectStore.createIndex('telefono', 'telefono', { unique: false });
+            objectStore.createIndex('empresa', 'empresa', { unique: false });
+            objectStore.createIndex('id', 'id', { unique: true });
+
+            console.log('DB LISTA Y CREADA');
+
+        }
+
+    }
+})();
+
