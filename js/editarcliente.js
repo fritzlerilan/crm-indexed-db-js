9d6aec3366a95ffe6a263af6ebfb84c0c1ed2a4d
fritzlerilan <fritzlerilan@gmail.com>
2021-10-02 13:20:36 -0300

    refactor - customer edit success

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,14 +1,17 @@
 (function () {
+    let idCliente;
     const nombreInput = document.querySelector('#nombre');
     const emailInput = document.querySelector('#email');
     const telefonoInput = document.querySelector('#telefono');
     const empresaInput = document.querySelector('#empresa');
 
+    const formulario = document.querySelector('#formulario');
+
     document.addEventListener('DOMContentLoaded', () => {
         // Verificar el ID de la URL
-
+        formulario.addEventListener('submit', actualizarCliente);
         const parametrosURL = new URLSearchParams(window.location.search);
-        const idCliente = parametrosURL.get('id');
+        idCliente = parametrosURL.get('id');
 
         if (idCliente) {
             conectarDB(db => { obtenerCliente(idCliente, db) });
@@ -20,11 +23,11 @@
         const objectStore = transaction.objectStore('crm');
 
         const cliente = objectStore.openCursor();
-        cliente.onsuccess = function(e){
+        cliente.onsuccess = function (e) {
             const cursor = e.target.result;
-            if(cursor){
+            if (cursor) {
                 const row = cursor.value;
-                if(row.id === Number(id)){
+                if (row.id === Number(id)) {
                     llenarForulario(row);
                 }
                 cursor.continue();
@@ -32,25 +35,48 @@
         }
     }
 
-    function llenarForulario(datosCliente){
-        const {nombre, email, telefono, empresa} = datosCliente;
+    function llenarForulario(datosCliente) {
+        const { nombre, email, telefono, empresa } = datosCliente;
         nombreInput.value = nombre;
         emailInput.value = email;
         telefonoInput.value = telefono;
         empresaInput.value = empresa;
     }
-    
-    function conectarDB(callback) {
-        const abrirConexion = window.indexedDB.open('crm', 1);
-        abrirConexion.onerror = () => {
-            console.log('Hubo un error');
+
+    function actualizarCliente(e) {
+        e.preventDefault();
+        if (nombreInput.value === '' || emailInput.value === '' || telefonoInput.value === '' || empresaInput.value === '') {
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+            return;
         }
-        abrirConexion.onsuccess = () => {
-            console.log('Conexion establecida');
-            const db = abrirConexion.result;
-            if (callback) {
-                return callback(db);
-            }
+
+        // Actualizar cliente
+
+        clienteActualizado = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: Number(idCliente)
+        }
+        conectarDB(db => putCliente(clienteActualizado, db));
+
+    }
+
+    function putCliente(cliente, db) {
+        const transaction = db.transaction('crm', 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.put(cliente);
+        transaction.oncomplete = () => {
+            imprimirAlerta('Editado correctamente');
+            formulario.reset();
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        }
+        transaction.onerror = () => {
+            imprimirAlerta('Hubo un error', 'error');
         }
     }
 })();
