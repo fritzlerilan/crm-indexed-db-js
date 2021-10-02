9efb54beb1456c16e5df1fea51318d1b7e355fee
fritzlerilan <fritzlerilan@gmail.com>
2021-10-02 12:26:32 -0300

    show customers script

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 (function () {
     let DB;
+    const tbodyListadoClientes = document.querySelector('#listado-clientes');
+    
     window.onload = () => {
-        crearDB();
+        crearDB(db => mostrarClientes(db));
     }
 
-    function crearDB() {
+    function crearDB(callback) {
         const crearDB = window.indexedDB.open('crm', 1);
         crearDB.onerror = () => {
             console.log('Hubo un error');
         }
 
-        crearDB.onsuccess = () => {
+        crearDB.onsuccess = (e) => {
             DB = crearDB.result;
+            if (callback) {
+                return callback(e.target.result);
+            }
         }
 
         crearDB.onupgradeneeded = function (e) {
@@ -27,6 +32,27 @@
 
             console.log('DB LISTA Y CREADA');
 
+        }
+
+    }
+
+    function mostrarClientes(db) {
+        // Obtener el cursor
+        const transaction = db.transaction('crm', 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.openCursor().onsuccess = (e) => {
+            let cursor = e.target.result;
+            if (cursor) {
+                const cliente = cursor.value;
+
+                // const clienteHTML = crearElementoCliente(cliente);
+                // 
+
+                cursor.continue();
+            }else {
+                console.log('Todas las entradas fueron recorridas');
+            }
         }
 
     }
