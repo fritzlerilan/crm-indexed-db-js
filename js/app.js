302a7737807b9561cfbab138295ac73535f6c34d
fritzlerilan <fritzlerilan@gmail.com>
2021-10-02 14:02:47 -0300

    Delete client

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 
     window.onload = () => {
         crearDB(db => mostrarClientes(db));
+        tbodyListadoClientes.addEventListener('click', eliminarRegistro);
     }
 
     function crearDB(callback) {
@@ -72,10 +73,41 @@
             </td>
             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
             </td>
         `
         return row;
+    }
+
+    function eliminarRegistro(e) {
+        if (e.target.classList.contains('eliminar')) {
+            const idEliminar = Number(e.target.dataset.cliente);
+
+            const confirmar = confirm('Deseas eliminar este cliente?');
+            if (confirmar) {
+                try {
+                    conectarDB(db => deleteClient(idEliminar, db))
+                    e.target.parentElement.parentElement.remove();
+                } catch (error) {
+                    imprimirAlerta('Hubo un error al eliminar el registro de la base de datos. Contactese con un administrador o reintente nuevamente.', error);
+                }
+            }
+            return;
+        }
+    }
+
+    function deleteClient(id, db) {
+        const transaction = db.transaction('crm', 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        
+        objectStore.delete(id);
+        transaction.oncomplete = (e) => {
+            console.log('Registro eliminado exitosamente', e)
+            
+        }
+        transaction.onerror = () => {
+            imprimirAlerta('Hubo un error al eliminar el registro de la base de datos. Contactese con un administrador o reintente nuevamente.', error);
+        }
     }
 })();
 
